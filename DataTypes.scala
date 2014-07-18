2ea15e3fdd31220ffa0cf8d0779a9f3959142865
Bill Frasure <bill.frasure@gmail.com>
2014-07-18 15:09:09 -0600

    Complete DataTypes atom
    Pretty simple stuff here. Just shows how to explicity choose DataTypes
    for your values.

---
+++ DataTypes.scala
@@ -0,0 +1,28 @@
+val number:Int = 5
+println("Number: " + number)
+
+val myString:String = "ABC1234"
+println("myString: " + myString)
+
+val myFloat:Double = 5.4
+println("myFloat: " + myFloat)
+
+val firstBoolean:Boolean = true
+println("firstBoolean: " + firstBoolean)
+
+val myLongString:String = """
+Aren't
+multiline
+strings
+lovely?
+"""
+println("myLongString: " + myLongString)
+
+// Error: Type mismatch
+//val invalidBoolean:Boolean = "maybe"
+
+// Error: Type mismatch
+//val rounded:Int = 15.4
+
+val floatFromInt:Float = 15
+println("floatFromInt: " + floatFromInt)
