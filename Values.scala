ba61380dca869328d3135a5447c30c9c1003816f
Bill Frasure <bill.frasure@gmail.com>
2014-07-18 14:57:30 -0600

    Complete Values atom
    Val's are constant. These will obviously be useful in functional
    situations.

---
+++ Values.scala
@@ -0,0 +1,17 @@
+val number = 17
+
+// Error for reassigning val
+//number = 20 
+
+println("Number: " + number)
+
+
+val myString = "ABC1234"
+
+// Same error as the one above.
+//myString = "DEF1234"
+
+println("myString: " + myString)
+
+val floatNumber = 15.56
+println("floatNumber: " + floatNumber)
