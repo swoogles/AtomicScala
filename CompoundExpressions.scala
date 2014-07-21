cfac6a07d1398667202b2de2f99252b1fced44d8
Bill Frasure <bill.frasure@gmail.com>
2014-07-21 14:53:22 -0600

    Complete CompoundExpressions atom
    I skipped the last question, because it was basically asking for a third
    part identical to the first and second.

--- CompoundExpressions.scala
+++ CompoundExpressions.scala
@@ -15,6 +15,37 @@ else if ( a <= c || b <= c ) {
   println("Either A or B is less than or equal to C.")
 }
 
+// CompoundExpressions3.scala
+val activity = "couch"
+val temperature = 40
+val hour = 10
+val isOpen = {
+  if(activity == "swimming" ) {
+      val opens = 9
+      val closes = 20
+      (hour >= opens && hour <= closes)
+  } else {
+    true
+  }
+}
+val goodTemperature = {
+  if (activity == "swimming") {
+    val minTemp = 55
+    val maxTemp = 150
+    (temperature >= minTemp && temperature <= maxTemp)
+  }
+  else if ( activity == "walking" ||
+            activity == "biking" )
+  {
+    val minTemp = 15
+    val maxTemp = 110
+    (temperature >= minTemp && temperature <= maxTemp)
+  }
+  else {
+    true
+    }
+}
+
 val doActivity = isOpen && goodTemperature
 println(activity + ":" + isOpen + " && " +
 goodTemperature + " = " + doActivity)
@@ -25,3 +56,4 @@ walking:true && true = true
 biking:true && false = false
 couch:true && true = true
 */
+
