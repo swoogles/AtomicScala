34261946583d7e141b10c8941799f4d2740a55ca
Bill Frasure <bill.frasure@gmail.com>
2014-07-22 09:54:48 -0600

    Complete CreatingClasses atom

---
+++ CreatingClasses.scala
@@ -0,0 +1,29 @@
+class Hippo
+class Lion
+class Tiger
+class Monkey
+class Giraffe
+class Zebra {
+  println("I have stripes.")
+}
+
+val hippo = new Hippo
+val lion = new Lion
+val tiger = new Tiger
+val monkey = new Monkey
+val giraffe = new Giraffe
+
+println("hippo: " + hippo )
+println("lion: " + lion )
+println("tiger: " + tiger )
+println("monkey: " + monkey )
+println("giraffe: " + giraffe )
+
+val lion2 = new Lion
+val giraffe2 = new Giraffe
+val giraffe3 = new Giraffe
+val zebra = new Zebra
+
+println("lion2: " + lion2 )
+println("giraffe2: " + giraffe2 )
+println("giraffe3: " + giraffe3 )
