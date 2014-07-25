984e0ceea102dad2fbf545099eedfeeba34245bf
Bill Frasure <bill.frasure@gmail.com>
2014-07-25 15:13:34 -0600

    Complete PatternMatching Atom

--- PatternMatching.scala
+++ PatternMatching.scala
@@ -81,24 +81,46 @@ checkTruthWithoutPattern(true && false,false && true) is "Both are false"
 def checkTruth( exp1:Boolean, exp2:Boolean):String = {
   (exp1, exp2) match {
     case (true, true) => "Both are true"
-    case (false,true) => "Both are false"
+    case (false,false) => "Both are false"
+    case (true,false) => "First: true, second: false" 
+    case (false,true) => "First: false, second: true"
   }
-
-  //if(exp1 && exp2) {
-  //  "Both are true"
-  //}
-  //else if(!exp1 && !exp2) {
-  //  "Both are false"
-  //}
-  //else if(exp1) {
-  //  "First: true, second: false"
-  //}
-  //else {
-  //  "First: false, second: true"
-  //}
 }
 
 checkTruth(true || false, true) is "Both are true"
 checkTruth(1 > 0 && -1 < 0, 1 == 2) is "First: true, second: false"
 checkTruth(1 >= 2, 1 >= 1) is "First: false, second: true"
 checkTruth(true && false,false && true) is "Both are false"
+
+
+//Create a method forecast that represents the percentage of
+//cloudiness, and use it to produce a “weather forecast” string such
+//as “Sunny” (100), “Mostly Sunny” (80), “Partly Sunny” (50), “Mostly
+//Cloudy” (20), and “Cloudy” (0). For this exercise, only match for the
+//legal values 100, 80, 50, 20, and 0 (we will revisit this exercise later).
+//Everything else should produce “Unknown.” Satisfy the following
+//tests:
+
+def forecast(temp:Int):String = {
+  temp match {
+    case 100 => "Sunny"
+    case 80 => "Mostly Sunny"
+    case 50 => "Partly Sunny"
+    case 20 => "Mostly Cloudy"
+    case 0 => "Cloudy"
+    case _ => "Unknown"
+  }
+}
+
+forecast(100) is "Sunny"
+forecast(80) is "Mostly Sunny"
+forecast(50) is "Partly Sunny"
+forecast(20) is "Mostly Cloudy"
+forecast(0) is "Cloudy"
+forecast(15) is "Unknown"
+
+val weatherData = Vector(100,80,50,20,0,15)
+
+for ( condition <- weatherData ) {
+  forecast(condition)
+}
